ce8fa39162d21faa74e3fbb039326c15cd305d7a
7minseok7 <mansang99@gmail.com>
2025-08-21 15:54:27 +0900

    fix: DetailView, ListView index.ts 수정

--- caregiver_web/app/components/Schedule/DetailView/index.ts
+++ caregiver_web/app/components/Schedule/DetailView/index.ts
@@ -1,1 +1,7 @@
-
+export { default as ScheduleDetailPage } from './ScheduleDetailPage';
+export { default as ApplicantInfoCard } from './ApplicantInfoCard';
+export { default as ContactInfoCard } from './ContactInfoCard';
+export { default as VisitLocationCard } from './VisitLocationCard';
+export { default as ApplicantDetailCard } from './ApplicantDetailCard';
+export { default as SpecialNotesCard } from './SpecialNotesCard';
+export { default as ActionButtons } from './ActionButtons';
--- caregiver_web/app/components/Schedule/index.ts
+++ caregiver_web/app/components/Schedule/index.ts
@@ -1,8 +1,14 @@
 export { default as ViewToggle } from './ViewToggle';
-export { default as ScheduleListView } from './ListView';
+export { ScheduleListView } from './ListView';
 export { default as ScheduleHeader } from './CalendarView/ScheduleHeader';
 export { default as ScheduleGridBody } from './CalendarView/ScheduleGridBody';
 export { default as ScheduleGrid } from './CalendarView/ScheduleGrid';
 
 // DetailView exports
-export * from './DetailView';
+export { default as ScheduleDetailPage } from './DetailView/ScheduleDetailPage';
+export { default as ApplicantInfoCard } from './DetailView/ApplicantInfoCard';
+export { default as ContactInfoCard } from './DetailView/ContactInfoCard';
+export { default as VisitLocationCard } from './DetailView/VisitLocationCard';
+export { default as ApplicantDetailCard } from './DetailView/ApplicantDetailCard';
+export { default as SpecialNotesCard } from './DetailView/SpecialNotesCard';
+export { default as ActionButtons } from './DetailView/ActionButtons';
