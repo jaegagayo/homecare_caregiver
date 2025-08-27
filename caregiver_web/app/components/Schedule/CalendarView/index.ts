4500ce061f9e5f9605036fe744d78d0acf8a661e
7minseok7 <mansang99@gmail.com>
2025-08-28 02:53:40 +0900

    chore: 불필요한 컴포넌트 export 제거

--- caregiver_web/app/components/Schedule/CalendarView/index.ts
+++ caregiver_web/app/components/Schedule/CalendarView/index.ts
@@ -1,3 +1,2 @@
 export { default as ScheduleHeader } from './ScheduleHeader';
 export { default as ScheduleGridBody } from './ScheduleGridBody';
-export { default as ScheduleGrid } from './ScheduleGrid';
